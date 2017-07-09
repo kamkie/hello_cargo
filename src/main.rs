bb8aecd71f3e67114964ddc51d3b1b40702ae1d7
Kamil Kiewisz <kamkie@outlook.com>
2017-07-09 13:56:37 +0200

    include server http

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,46 @@
 use std::ffi::OsString;
+use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
+use std::str::FromStr;
+
+extern crate iron;
+extern crate time;
+
+use iron::prelude::*;
+use iron::{BeforeMiddleware, AfterMiddleware, typemap};
+use time::precise_time_ns;
+
+struct ResponseTime;
+
+impl typemap::Key for ResponseTime {
+    type Value = u64;
+}
+
+impl BeforeMiddleware for ResponseTime {
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        req.extensions.insert::<ResponseTime>(precise_time_ns());
+        Ok(())
+    }
+}
+
+impl AfterMiddleware for ResponseTime {
+    fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
+        let delta = precise_time_ns() - *req.extensions.get::<ResponseTime>().unwrap();
+        println!("Request took: {} ms", (delta as f64) / 1000000.0);
+        Ok(res)
+    }
+}
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Ok, "Hello World")))
+}
+
+fn start_server() {
+    let mut chain = Chain::new(hello_world);
+    chain.link_before(ResponseTime);
+    chain.link_after(ResponseTime);
+    Iron::new(chain).http("localhost:3000").unwrap();
+}
 
 fn main() {
     let x = "Hello, world!";
@@ -24,6 +66,14 @@ fn main() {
     assert_eq!(Some(1).unwrap_or(-1), 1);
     assert_eq!(None.unwrap_or("bike"), "bike");
     assert_eq!(None.unwrap_or(-1), -1);
+
+    let ip: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+    let ipv6 = Ipv6Addr::from_str("::1").unwrap();
+    println!("ip: {}", ip.is_loopback());
+    println!("ip2: {}", ip.to_ipv6_mapped().is_loopback());
+    println!("ip3: {}", ipv6.is_loopback());
+
+    start_server();
 }
 
 fn get_or_default(ref vec: &Vec<i32>, index: usize, default: i32) -> i32 {
